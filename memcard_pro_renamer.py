473a7b55ee7fbd8787c3f1030657f152ec67fa7d
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-10 18:08:25 -0700

    Added memcard pro renamer (need to check issues with some entries in database)

---
+++ memcard_pro_renamer.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python3
+'''
+Manage save files from MemCardPro
+'''
+from datetime import datetime
+from glob import glob
+from json import load as jload
+from os import mkdir
+from os.path import isdir, isfile
+from shutil import copytree
+from sys import argv
+from urllib.request import urlopen
+
+# serial-to-title JSON URLs
+JSON_URLS = {
+    'PSX': 'https://github.com/niemasd/PSX-PS2-Save-Tools/raw/main/data/PSX.json',
+    'PS2': 'https://github.com/niemasd/PSX-PS2-Save-Tools/raw/main/data/PS2.json',
+}
+
+# folders to ignore
+IGNORE = {'__MACOSX'}
+
+# get current time as a string YYYY-MM-DD_HH-MM-SS
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# load serial-to-title mapping
+def load_jsons(json_urls):
+    out = dict()
+    for url in json_urls.values():
+        try:
+            curr = jload(urlopen(url))
+        except:
+            raise RuntimeError("Failed to load: %s" % url)
+        for k,v in curr.items():
+            out[k.strip().upper()] = v.strip().upper()
+    return out
+
+# main program
+if __name__ == "__main__":
+    # check user args
+    if len(argv) != 2:
+        print("%s <folder>" % argv[0]); exit(1)
+    if not isdir(argv[1]):
+        raise ValueError("Folder not found: %s" % argv[1])
+
+    # load JSONs
+    print("Loading mapping of serial numbers to titles...")
+    TITLE = load_jsons(JSON_URLS)
+
+    # find game folders via DFS
+    print("Scanning for game folders in: %s" % argv[1])
+    game_folders = set(); to_explore = [argv[1]]
+    while len(to_explore) != 0:
+        curr_path = to_explore.pop()
+        if isdir(curr_path) and curr_path not in IGNORE:
+            to_explore += list(glob('%s/*' % curr_path))
+        elif isfile(curr_path) and curr_path.lower().endswith('.mcd'):
+            game_folders.add('/'.join(curr_path.split('/')[:-1]))
+    print("Found %d game folder(s)" % len(game_folders))
+
+    # rename game folders
+    out_path = '%s/../renamed_%s' % (argv[1], get_time())
+    if isfile(out_path) or isdir(out_path):
+        raise ValueError("Output path exists: %s" % out_path)
+    mkdir(out_path); serial_not_found = list()
+    for curr_path in game_folders:
+        folder = curr_path.split('/')[-1].strip()
+        if ' - ' in folder: # folder with title, so rename to MemCard PRO format
+            memcard_folder = '-'.join(list(glob('%s/*.mcd' % curr_path))[0].split('-'))
+            copytree(curr_path, '%s/%s' % (out_path, memcard_folder))
+        else:               # folder without title, so rename to human-readable
+            folder_upper = folder.upper(); curr_title = None
+            if folder_upper in TITLE:
+                curr_title = TITLE[folder_upper]
+            else: # brute force search
+                for serial, title in TITLE.items():
+                    if serial.upper() in folder_upper:
+                        curr_title = title; break
+            if curr_title is None:
+                serial_not_found.append(folder_upper)
+            else:
+                copytree(curr_path, '%s/%s - %s' % (out_path, folder, curr_title))
+
+    # report any that failed to copy
+    if len(serial_not_found) != 0:
+        print("\nThe following serial numbers were not found in the database and were skipped:\n\n%s" % '\n'.join(serial_not_found))
+        print("\nConsider submitting them to https://psxdatacenter.com")
+        input("\nPress ENTER to exit")
